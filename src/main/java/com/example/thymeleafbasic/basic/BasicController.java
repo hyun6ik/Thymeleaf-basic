d53bd88c0f9fd74b3a070d9d24da164039309904
DESKTOP-NQ23D5Q\Hyunsik <fkanwm@phpschool.com>
2021-06-26 15:43:57 +0900

    변수- SpringEL

--- src/main/java/com/example/thymeleafbasic/basic/BasicController.java
+++ src/main/java/com/example/thymeleafbasic/basic/BasicController.java
@@ -1,9 +1,14 @@
 package com.example.thymeleafbasic.basic;
 
+import com.example.thymeleafbasic.basic.dto.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic")
@@ -21,5 +26,26 @@ public class BasicController {
         model.addAttribute("data", "Hello Spring!");
         return "basic/unescaped";
     }
+
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User().builder().username("userA").age(10).build();
+        User userB = new User().builder().username("userB").age(20).build();
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        HashMap<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
 
 }
---
+++ src/main/java/com/example/thymeleafbasic/basic/dto/User.java
@@ -0,0 +1,16 @@
+package com.example.thymeleafbasic.basic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    private String username;
+    private int age;
+}
